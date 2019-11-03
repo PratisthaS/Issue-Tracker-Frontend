d53a6da4a944167cbd3915721c0f28c9052014f4
shresthasumit55 <shresthasumit55@gmail.com>
2019-11-03 17:12:50 -0600

    UI structure changes

---
+++ src/components/Issues.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import axios from 'axios';
+import DataTable from 'react-data-table-component';
+import AppBar from '@material-ui/core/AppBar';
+import {
+  FormControl,
+  InputLabel,
+  Input,
+  Button,
+  TextField
+} from "@material-ui/core";
+
+export default class Issues extends React.Component {
+
+  
+    render() {
+
+      return (
+        <div>
+          Issue page
+        </div>
+
+
+      )
+    }
+  }
--- src/index.js
+++ src/index.js
@@ -3,8 +3,36 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import './index.css'
+import { Route, Link, BrowserRouter as Router } from 'react-router-dom'
+import Project from './components/Project'
+import Issues from './components/Issues'
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const routing = (
+    <Router>
+
+        <ul>
+        <li>
+          <Link to="/">Home</Link>
+        </li>
+        <li>
+          <Link to="/project">Project</Link>
+        </li>
+        <li>
+          <Link to="/issue">Issues</Link>
+        </li>
+      </ul>
+
+      <div>
+        <Route exact path="/" component={App} />
+        <Route path="/project" component={Project} />
+        <Route path="/issue" component={Issues} />
+
+      </div>
+    </Router>
+  )
+  ReactDOM.render(routing, document.getElementById('root'))
+
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
